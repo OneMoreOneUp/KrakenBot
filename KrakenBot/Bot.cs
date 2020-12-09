7eb9a1fd0c9664f6538f2487ae78775d1fbf69d0
OneMoreOneUp <matthew@linoak.com>
2020-12-09 04:18:36 -0500

    Initial setup

---
+++ KrakenBot/Bot.cs
@@ -0,0 +1,43 @@
+﻿using DSharpPlus;
+using DSharpPlus.CommandsNext;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KrakenBot
+{
+    public class Bot
+    {
+        public DiscordClient Client { get; private set; }
+        public CommandsNextExtension Commands { get; private set; }
+
+        public async Task RunAsync()
+        {
+            DiscordConfiguration config = new DiscordConfiguration
+            {
+
+            };
+
+            Client = new DiscordClient(config);
+
+            Client.Ready += ClientReady;
+
+            CommandsNextConfiguration commandsConfig = new CommandsNextConfiguration
+            {
+
+            };
+
+            Commands = Client.UseCommandsNext(commandsConfig);
+
+            await Client.ConnectAsync();
+
+            await Task.Delay(1);    //Prevents early quit from bot
+        }
+
+        private Task ClientReady(DiscordClient sender, DSharpPlus.EventArgs.ReadyEventArgs e)
+        {
+            return null;
+        }
+    }
+}
---
+++ KrakenBot/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace KrakenBot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Bot bot = new Bot();
+        }
+    }
+}
